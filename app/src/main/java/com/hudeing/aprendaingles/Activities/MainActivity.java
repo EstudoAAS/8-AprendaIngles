f688256d7e1feb7737ba3cad323f557431306467
Aller Augusto de Souza <aller.souza@samsung.com>
2019-01-25 16:39:43 -0200

    Add full source code

---
+++ app/src/main/java/com/hudeing/aprendaingles/Activities/MainActivity.java
@@ -0,0 +1,46 @@
+package com.hudeing.aprendaingles.Activities;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hudeing.aprendaingles.Fragments.BichosFragment;
+import com.hudeing.aprendaingles.Fragments.NumerosFragment;
+import com.hudeing.aprendaingles.Fragments.VogaisFragment;
+import com.hudeing.aprendaingles.R;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.smartTabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        //Aplica configurações na Action Bar
+        getSupportActionBar().setElevation(0);
+        getSupportActionBar().setTitle(R.string.app_name);
+
+        //Configurar abas
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add(R.string.str_bichos, BichosFragment.class)
+                .add(R.string.str_numeros, NumerosFragment.class)
+                //.add(R.string.str_vogais, VogaisFragment.class)
+                .create()
+        );
+
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/hudeing/aprendaingles/Fragments/NumerosFragment.java
@@ -0,0 +1,97 @@
+package com.hudeing.aprendaingles.Fragments;
+
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.hudeing.aprendaingles.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NumerosFragment extends Fragment implements View.OnClickListener {
+    private ImageButton buttonUm, buttonDois, buttonTres,
+                        buttonQuatro, buttonCinco, buttonSeis;
+    private MediaPlayer mediaPlayer;
+
+
+    public NumerosFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_numeros, container, false);
+
+        buttonUm = view.findViewById(R.id.buttonUm);
+        buttonDois = view.findViewById(R.id.buttonDois);
+        buttonTres = view.findViewById(R.id.buttonTres);
+        buttonQuatro = view.findViewById(R.id.buttonQuatro);
+        buttonCinco = view.findViewById(R.id.buttonCinco);
+        buttonSeis = view.findViewById(R.id.buttonSeis);
+
+        buttonUm.setOnClickListener(this);
+        buttonDois.setOnClickListener(this);
+        buttonTres.setOnClickListener(this);
+        buttonQuatro.setOnClickListener(this);
+        buttonCinco.setOnClickListener(this);
+        buttonSeis.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonUm:
+                tocarSom(R.raw.one);
+                break;
+            case R.id.buttonDois:
+                tocarSom(R.raw.two);
+                break;
+            case R.id.buttonTres:
+                tocarSom(R.raw.three);
+                break;
+            case R.id.buttonQuatro:
+                tocarSom(R.raw.four);
+                break;
+            case R.id.buttonCinco:
+                tocarSom(R.raw.five);
+                break;
+            case R.id.buttonSeis:
+                tocarSom(R.raw.six);
+                break;
+        }
+    }
+
+    public void tocarSom(int raw) {
+        mediaPlayer = MediaPlayer.create(getActivity(),raw);
+        mediaPlayer.start();
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mediaPlayer.release();
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer =null;
+        }
+    }
+}
